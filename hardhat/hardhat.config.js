bd4b5bb1d81c28e6cbbb5e00eb8d2de5f952b4ee
Joaquin-VM-TFG <217283136+Joaquin-VM-TFG@users.noreply.github.com>
2025-06-21 00:25:14 -0300

    Subida de la demo del prototipo funcional del TFG
    Se suben los proyectos que componen la demo del prototipo funcional del Trabajo Final de Grado (TFG), desarrollados para una plataforma de juego con economía tokenizada implementada en blockchain. El proyecto hardhat contiene los contratos inteligentes en Solidity que gestionan la lógica del sistema, mientras que el proyecto frontend incluye la interfaz de usuario que permite interactuar con dicha plataforma.

---
+++ hardhat/hardhat.config.js
@@ -0,0 +1,26 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@openzeppelin/hardhat-upgrades");
+require("dotenv").config();
+
+module.exports = {
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+  },
+  paths: {
+    artifacts: "./artifacts",
+    cache: "./cache",
+    sources: "./contracts",
+    tests: "./test",
+  },
+};
